a46601b62b69f0e800b80e1fd526c69f55ed309c
Amadu James <banguralamin616@gmail.com>
2024-08-21 10:13:09 +0000

    Add files via upload

---
+++ Python Training/user_input.py.py
@@ -0,0 +1,7 @@
+#Asking for a name
+UserName = input("Enter your name:")
+#Asking for a age
+Age = input("Enter your age:")
+#Asking for a location
+Location = input("Enter your Location:")
+print(f'Hello! {UserName},you are {Age} years old and live in {Location}.')
